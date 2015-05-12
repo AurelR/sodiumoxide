477cc1b430e252fc910788de0177d92b238d1cf1
AurelR <aurel.rothaermel@gmail.com>
2015-05-12 16:14:47 +0200

    Add bindings to crypto_generichash_blake2b.h (all parts)

--- libsodium-sys/src/crypto_generichash_blake2b.rs
+++ libsodium-sys/src/crypto_generichash_blake2b.rs
@@ -1,5 +1,21 @@
 // crypto_generichash_blake2b.h
 
+// TODO: ALIGN(64)
+#[repr(C, packed)]
+#[derive(Copy)]
+pub struct crypto_generichash_blake2b_state {
+    h: [u64; 8],
+    t: [u64; 2],
+    f: [u64; 2],
+    buf: [u8; 2 * 128],
+    buflen: size_t,
+    last_node: u8,
+}
+
+impl Clone for crypto_generichash_blake2b_state {
+    fn clone(&self) -> crypto_generichash_blake2b_state { *self }
+}
+
 pub const crypto_generichash_blake2b_BYTES_MIN: usize = 16;
 pub const crypto_generichash_blake2b_BYTES_MAX: usize = 64;
 pub const crypto_generichash_blake2b_BYTES: usize = 32;
@@ -8,7 +24,6 @@ pub const crypto_generichash_blake2b_KEYBYTES_MAX: usize = 64;
 pub const crypto_generichash_blake2b_KEYBYTES: usize = 32;
 pub const crypto_generichash_blake2b_SALTBYTES: usize = 16;
 pub const crypto_generichash_blake2b_PERSONALBYTES: usize = 16;
-
 
 extern {
     pub fn crypto_generichash_blake2b_bytes_min() -> size_t;
@@ -39,6 +54,33 @@ extern {
         salt: *const [u8; crypto_generichash_blake2b_SALTBYTES],
         personal: *const [u8; crypto_generichash_blake2b_PERSONALBYTES])
         -> c_int;
+
+    pub fn crypto_generichash_blake2b_init(
+        state: *mut crypto_generichash_blake2b_state,
+        key: *const u8,
+        keylen: size_t,
+        outlen: size_t)
+        -> c_int;
+
+    pub fn crypto_generichash_blake2b_init_salt_personal(
+        state: *mut crypto_generichash_blake2b_state,
+        key: *const u8,
+        keylen: size_t,
+        outlen: size_t,
+        salt: *const [u8; crypto_generichash_blake2b_SALTBYTES],
+        personal: *const [u8; crypto_generichash_blake2b_PERSONALBYTES])
+        -> c_int;
+
+    pub fn crypto_generichash_blake2b_update(
+        state: *mut crypto_generichash_blake2b_state,
+        in_: *const u8,
+        inlen: c_ulonglong)
+        -> c_int;
+
+    pub fn crypto_generichash_blake2b_final(
+        state: *mut crypto_generichash_blake2b_state,
+        out: *mut u8,
+        outlen: size_t) -> c_int;
 }
 
 #[test]
